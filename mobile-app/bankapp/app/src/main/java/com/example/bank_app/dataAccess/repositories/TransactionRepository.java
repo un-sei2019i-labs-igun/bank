f91d8a30449e7278e9d2b501894f162f065dc0ee
Rin <rin@Garys-MacBook-Pro.local>
2019-06-08 18:12:56 -0500

    update

--- mobile-app/bankapp/app/src/main/java/com/example/bank_app/dataAccess/repositories/TransactionRepository.java
+++ mobile-app/bankapp/app/src/main/java/com/example/bank_app/dataAccess/repositories/TransactionRepository.java
@@ -1,19 +1,34 @@
 package com.example.bank_app.dataAccess.repositories;
 
+import com.example.bank_app.dataAccess.models.Account;
 import com.example.bank_app.dataAccess.models.Transaction;
+import com.example.bank_app.dataAccess.models.TransactionDao;
+import com.example.bank_app.dataAccess.models.UserDao;
+
 
 public class TransactionRepository extends Repository{
 
-    public Transaction create(){
-        return null;
+
+     TransactionDao transactionDao;
+
+
+    public TransactionRepository(TransactionDao transactionDao){
+        this.transactionDao = transactionDao;
     }
 
-    public Transaction getById(){
-        return null;
+
+    public Transaction create(Transaction transaction){
+       transactionDao.insert(transaction);
+       return transaction;
     }
 
-    public Transaction update(){
-        return null;
+    public Transaction getById(long id){
+      return  transactionDao.load(id);
+    }
+
+    public Transaction update(Transaction transaction){
+        transactionDao.update(transaction);
+        return transaction;
     }
 
 }
