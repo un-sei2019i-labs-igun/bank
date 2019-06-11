dc927641e461d47862ec16cf49e3a974266ffd50
icquirogac <icquirogac@unal.edu.co>
2019-06-10 22:40:51 -0500

    Activities creadas

--- mobile-app/bankapp/app/src/main/java/com/example/bank_app/presentation/MainActivity.java
+++ mobile-app/bankapp/app/src/main/java/com/example/bank_app/presentation/MainActivity.java
@@ -1,4 +1,14 @@
 package com.example.bank_app.presentation;
 
-public class MainActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.bank_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
--- mobile-app/bankapp/app/src/main/java/com/example/bank_app/presentation/SendMoneyActivity.java
+++ mobile-app/bankapp/app/src/main/java/com/example/bank_app/presentation/SendMoneyActivity.java
@@ -1,4 +1,14 @@
 package com.example.bank_app.presentation;
 
-public class SendMoneyActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.bank_app.R;
+
+public class SendMoneyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_money);
+    }
 }
